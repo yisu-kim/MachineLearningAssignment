81e182852739d19f0ff69ff77552d9db79fd5f29
yisu-kim <ykim6600@gmail.com>
2018-12-01 21:38:37 +0900

    implement cost function reg

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
 
+theta = theta(2:length(theta));
+J = (1 / m) * (-y' * log(h) - (1 - y') * log(1 - h)) + (lambda / (2 * m)) * (theta' * theta);
 
-
-
+theta = [0; theta];
+grad = (1 / m) * (h - y)' * X + (lambda / m) * theta';
 
 % =============================================================
 
