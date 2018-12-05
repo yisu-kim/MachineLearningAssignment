c95be55a58d93c728bdd4267c1fa88a28f92054b
yisu-kim <ykim6600@gmail.com>
2018-12-05 20:57:45 +0900

    Implement predict

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,22 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% a0
+a0 = ones(m, 1);
 
+% Input layer
+a1 = [a0 X];
 
+% Hidden layer
+z2 = a1 * Theta1';
+a2 = [a0 sigmoid(z2)];
 
+% Output layer
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
-
-
-
+[Y, I] = max(a3, [], 2);
+p = I;
 
 % =========================================================================
 
