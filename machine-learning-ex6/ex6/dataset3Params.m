4bba224d5e434f23e1fef5aed0a206284b068af0
yisu-kim <ykim6600@gmail.com>
2018-12-29 19:51:37 +0900

    Parameters (C, sigma) for Dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+parameters = [0.01 0.03 0.1 0.3 1 3 10 30];
+minError = Inf;
 
-
-
-
-
+for c = parameters
+    for s = parameters
+        model= svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        
+        if error < minError
+            minError = error;
+            C = c;
+            sigma = s;
+        end
+    end
+end
 
 % =========================================================================
 
